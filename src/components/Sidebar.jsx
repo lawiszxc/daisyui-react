769674e985f585797cebc3f4d085ad6dd27a6ddd
Mark Laurence Lawis <lawis32918@gmail.com>
2025-07-07 14:10:35 +0800

    testing commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
+
 const Sidebar = () => {
+  const [activeMenu, setActiveMenu] = useState("baby-sitters");
+
   return (
-        <aside className="flex sm:flex md:flex flex-col w-64 bg-gray-800">
+    <aside className="flex sm:flex md:flex flex-col w-64 bg-gray-800">
       <div className="flex items-center justify-center h-24 bg-gray-900 p-4">
         <span className="text-white font-bold uppercase flex">
           <img className="h-19" src="/logo.png" alt="OCCLogo" />
@@ -11,6 +15,30 @@ const Sidebar = () => {
       </div>
       <div className="flex flex-col flex-1 overflow-y-auto">
         <div className="flex-1 px-2 py-4 bg-gray-800">
+          <a
+            href="#"
+            onClick={() => setActiveMenu("baby-sitters")}
+            className={`flex items-center px-4 py-2 hover:bg-gray-700 ${
+              activeMenu === "baby-sitters"
+                ? "bg-gray-700 text-white font-semibold"
+                : "text-gray-100"
+            }`}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 mr-2"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z"
+              />
+            </svg>
+            Baby Sitters
+          </a>
           <a
             href="#"
             className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700"
@@ -30,7 +58,7 @@ const Sidebar = () => {
             </svg>
             Baby Sitters
           </a>
-                    <a
+          <a
             href="#"
             className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700"
           >
@@ -49,7 +77,7 @@ const Sidebar = () => {
             </svg>
             Baby Sitters
           </a>
-                    <a
+          <a
             href="#"
             className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700"
           >
@@ -68,7 +96,7 @@ const Sidebar = () => {
             </svg>
             Baby Sitters
           </a>
-                    <a
+          <a
             href="#"
             className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700"
           >
@@ -87,7 +115,7 @@ const Sidebar = () => {
             </svg>
             Baby Sitters
           </a>
-                    <a
+          <a
             href="#"
             className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700"
           >
@@ -106,7 +134,7 @@ const Sidebar = () => {
             </svg>
             Baby Sitters
           </a>
-                    <a
+          <a
             href="#"
             className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700"
           >
@@ -125,7 +153,7 @@ const Sidebar = () => {
             </svg>
             Baby Sitters
           </a>
-                    <a
+          <a
             href="#"
             className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700"
           >
@@ -144,7 +172,7 @@ const Sidebar = () => {
             </svg>
             Baby Sitters
           </a>
-                    <a
+          <a
             href="#"
             className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700"
           >
@@ -163,7 +191,7 @@ const Sidebar = () => {
             </svg>
             Baby Sitters
           </a>
-                    <a
+          <a
             href="#"
             className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700"
           >
@@ -182,7 +210,7 @@ const Sidebar = () => {
             </svg>
             Baby Sitters
           </a>
-                    <a
+          <a
             href="#"
             className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700"
           >
@@ -201,7 +229,7 @@ const Sidebar = () => {
             </svg>
             Baby Sitters
           </a>
-                    <a
+          <a
             href="#"
             className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700"
           >
@@ -220,7 +248,7 @@ const Sidebar = () => {
             </svg>
             Baby Sitters
           </a>
-                    <a
+          <a
             href="#"
             className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700"
           >
@@ -239,7 +267,7 @@ const Sidebar = () => {
             </svg>
             Baby Sitters
           </a>
-                    <a
+          <a
             href="#"
             className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700"
           >
@@ -258,7 +286,7 @@ const Sidebar = () => {
             </svg>
             Baby Sitters
           </a>
-                    <a
+          <a
             href="#"
             className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700"
           >
@@ -277,7 +305,7 @@ const Sidebar = () => {
             </svg>
             Baby Sitters
           </a>
-                    <a
+          <a
             href="#"
             className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700"
           >
@@ -296,7 +324,7 @@ const Sidebar = () => {
             </svg>
             Baby Sitters
           </a>
-                    <a
+          <a
             href="#"
             className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700"
           >
@@ -315,7 +343,7 @@ const Sidebar = () => {
             </svg>
             Baby Sitters
           </a>
-                    <a
+          <a
             href="#"
             className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700"
           >
@@ -334,7 +362,7 @@ const Sidebar = () => {
             </svg>
             Baby Sitters
           </a>
-                    <a
+          <a
             href="#"
             className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700"
           >
@@ -353,7 +381,7 @@ const Sidebar = () => {
             </svg>
             Baby Sitters
           </a>
-                    <a
+          <a
             href="#"
             className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700"
           >
@@ -372,26 +400,7 @@ const Sidebar = () => {
             </svg>
             Baby Sitters
           </a>
-                    <a
-            href="#"
-            className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 mr-2"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z"
-              />
-            </svg>
-            Baby Sitters
-          </a>
-                    <a
+          <a
             href="#"
             className="flex items-center px-4 py-2 text-gray-100 hover:bg-gray-700"
           >
@@ -489,7 +498,8 @@ const Sidebar = () => {
           </a>
         </div>
       </div>
-    </aside>  )
-}
+    </aside>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
